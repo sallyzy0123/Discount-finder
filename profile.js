62f3881c682dafa88da50da0f5c2d737f22b9b0e
yingzh <Ying.Zhang@metropolia.fi>
2022-12-05 12:29:35 +0200

    push user profile page and bookmark page

---
+++ profile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// get username from database
+
+// get useremail from database
+
+// edit button to edit profile page
+
+// nav bar to different page
+
+// get gallery from database
+
+// gallery edit to edit post page
+
+// gallery bin to delete the post
