6d6ecfbdd16822ffaba0dc468b4d4c4e30d79754
darjapo <darja.polevaja@metropolia.fi>
2022-12-07 00:18:12 +0200

    wip: js for showing comments from json

---
+++ js/post.js
@@ -0,0 +1,58 @@
+const comments = [
+    {
+        'text': 'Nunc commodo faucibus arcu eu tristique. Duis iaculis tincidunt fermentum. Curabitur efficitur hendrerit justo, non tincidunt lectus lacinia et. Ut tincidunt placerat metus vitae rhoncus. Integer pretium at tellus vel aliquet. Nullam tincidunt congue lectus sed vulputate. Mauris auctor sem vel ante egestas sagittis. In facilisis odio accumsan iaculis volutpat.',
+        'date': '2022-11-11',
+        'username': 'sweetheart',
+        'img': '../images/profile.jpg',
+    },
+    {
+        'text': 'Nunc commodo faucibus arcu eu tristique. Duis iaculis tincidunt fermentum. Curabitur efficitur hendrerit justo, non tincidunt lectus lacinia et. Ut tincidunt placerat metus vitae rhoncus. Integer pretium at tellus vel aliquet. Nullam tincidunt congue lectus sed vulputate. Mauris auctor sem vel ante egestas sagittis. In facilisis odio accumsan iaculis volutpat.',
+        'date': '2022-12-10',
+        'username': 'pomegranate',
+        'img': '../images/profile.jpg',
+    },
+    {
+        'text': 'Nunc commodo faucibus arcu eu tristique. Duis iaculis tincidunt fermentum. Curabitur efficitur hendrerit justo, non tincidunt lectus lacinia et. Ut tincidunt placerat metus vitae rhoncus. Integer pretium at tellus vel aliquet. Nullam tincidunt congue lectus sed vulputate. Mauris auctor sem vel ante egestas sagittis. In facilisis odio accumsan iaculis volutpat.',
+        'date': '2022-09-13',
+        'username': 'iamtired',
+        'img': '../images/profile.jpg',
+    },
+]
+
+const commentSection = document.querySelector('.comment-section');
+
+for (let i = 0; i < comments.length; i++) {
+    const newComment = document.createElement('div');
+    newComment.className = "new-comment";
+
+    const commentHeader = document.createElement('div');
+    commentHeader.className = "comment-header";
+    const commentUser = document.createElement('div');
+    commentUser.className = "comment-user";
+    const commentUserPic = document.createElement('img');
+
+    const commentInfo = document.createElement('div');
+    commentInfo.className = "comment-info";
+    const commentDetails = document.createElement('div');
+    commentDetails.className = "comment-details";
+    const commentText = document.createElement('div');
+    commentText.className = "comment-text";
+    const commentDate = document.createElement('div');
+    commentDate.className = "comment-date";
+    const commentUsername = document.createElement('div');
+    commentUsername.className = "comment-username";
+
+    commentDetails.append(commentUsername, commentDate)
+    commentInfo.append(commentDetails, commentText);
+    commentUser.append(commentUserPic);
+    commentHeader.append(commentUser);
+    newComment.append(commentHeader, commentInfo);
+    commentSection.append(newComment);
+
+    commentText.textContent = comments[i].text;
+    commentDate.textContent = comments[i].date;
+    commentUsername.textContent = comments[i].username;
+    commentUserPic.src = comments[i].img;
+};
+
+console.log(comments);
