30348c9a1d7c1dc1882c56d4eabbdde9044ef129
yingzh <Ying.Zhang@metropolia.fi>
2022-12-07 13:08:00 +0200

    finish the profile and bookmark frontend

--- js/profile.js
+++ js/profile.js
@@ -9,30 +9,7 @@
 // nav bar to different page
 
 // get gallery from database
-const posts = [
-    {
-      'name': 'Title 1',
-      'location': 'Caption 1',
-      'originalprice': '150€',
-      'discountedprice':'100€',
-      'picture': 'images/discount1.png',
-    },
-    {
-  
-        'name': 'Title 2',
-        'location': 'Caption 2',
-        'originalprice': '2000€',
-        'discountedprice':'10000€',
-        'picture': 'images/discount1.png',
-    },
-    {
-        'name': 'Title 3',
-        'location': 'Caption 3',
-        'originalprice': '100€',
-        'discountedprice':'80€',
-        'picture': 'images/discount1.png',
-    },
-]
+
 // select existing html elements
 const gallery = document.querySelector('.gallery');
 
