d19d4ae9d97f06c0feb6811de08a8f1cc6c8720e
darjapo <darjapo@metropolia.fi>
2022-12-09 01:14:45 +0200

    wip: post controller, route and model, database connected, .env file and .gitignore line for it

--- server/app.js
+++ server/app.js
@@ -1,11 +1,5 @@
 'use strict';
 const express = require('express');
 const app = express();
-// const galleryRouter = require('../routes/galleryRoute');
 const port = 3000;
 
-app.get('/gallery', (req, res) => {
-    res.send('From this endpoint you can get users.')
-  });
-// app.use('/gallery', galleryRouter);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ server/models/postModel.js
@@ -0,0 +1,91 @@
+"use strict"
+const pool = require("../database/db");
+const promisePool = pool.promise();
+
+const getAllPosts = async (res) => {
+    try {
+        // const [rows] = await promisePool.
+        // query("SELECT user_id, name, email, role FROM wop_user");
+        // return rows;
+    } catch (e) {
+        console.error("error", e.message);
+        res.status(500).send(e.message);
+    }
+};
+
+const getPostById = async (res, postId) => {
+    try {
+        const [rows] = await promisePool.
+        query('select CategoryId, Name, Description, Location, Picture, OriginalPrice, DiscountedPrice ' +
+            'from Post ' +
+            'where PostId like ?',
+            [postId]);
+        return rows[0];
+    } catch (e) {
+        console.error("error", e.message);
+        res.status(500).send(e.message);
+    }
+};
+
+const addPost = async (post, res) => {
+    try {
+        const sql = 'insert into ' +
+            'Post(UserId, CategoryId, Name, Description, Location, Picture, OriginalPrice, DiscountedPrice)' +
+            'values (?, ?, ?, ?, ?, ?, ?, ?);';
+        const values = [];
+        const [result] = await promisePool.query(sql, values);
+        return result.insertId;
+    } catch (e) {
+        console.error("error", e.message);
+        res.status(500).send(e.message);
+    }
+};
+
+const deletePostById = async (postId, user, role, res) => {
+    try {
+        if (role === 0) {
+            const [rows] = await promisePool.
+            query('delete from Post where PostId = ?', [postId]);
+            return rows;
+        } else {
+            const [rows] = await promisePool.
+            query("delete from Post where PostId = ? and user = ?", [postId, user]);
+            return rows;
+        }
+    } catch (e) {
+        console.error("error", e.message);
+        res.status(500).send(e.message);
+    }
+};
+
+const updatePostById = async (post, user, role, res) => {
+    try {
+        console.log('Modifying post:', post);
+        if (role === 0) {
+            const sql = 'update Post set ' +
+                'CategoryId = ?, Name = ?, Description = ?, Location = ?, Picture = ?, OriginalPrice = ?, DiscountedPrice = ?' +
+                'where PostId = ?';
+            const values = [post.categoryId, post.name, post.description, post.location, post.picture, post.origPrice, post.discPrice, post.id];
+            const [rows] = await promisePool.query(sql, values)
+            return rows;
+        } else {
+            const sql = 'update Post set ' +
+                'CategoryId = ?, Name = ?, Description = ?, Location = ?, Picture = ?, OriginalPrice = ?, DiscountedPrice = ?' +
+                'where PostId = ? and userId = ?';
+            const values = [post.categoryId, post.name, post.description, post.location, post.picture, post.origPrice, post.discPrice, post.id, user.userId];
+            const [rows] = await promisePool.query(sql, values);
+            return rows;
+        }
+    } catch (e) {
+        console.error("error", e.message);
+        res.status(500).send(e.message);
+    }
+};
+
+module.exports = {
+    getAllPosts,
+    getPostById,
+    addPost,
+    deletePostById,
+    updatePostById
+};
